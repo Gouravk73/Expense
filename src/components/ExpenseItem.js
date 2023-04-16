8f322edfe6dca37110cb85f39dea414d18ba4034
Gourav <Gouravkumar.1273@gmail.com>
2023-04-16 07:51:29 +0530

    loop through elements

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import "./ExpenseItem.css";
-const ExpenseItem = () => {
-  const expenseDate=new Date(2021,2,28);
-  const expenseTitle='Car Insurane';
-  const expenseAmount=294.67;
+const ExpenseItem = (props ) => {
+  //const expenseDate=new Date(2021,2,28);
+  //const expenseTitle='Car Insurane';
+ // const expenseAmount=294.67;
   const LocationOfExpenditure='Delhi';
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toString()}</div>
+      <div>{props.date}</div>
        <div className="expense-item__description">
-        <h2>{expenseTitle} </h2>
+        <h2>{props.title} </h2>
         <h3>{LocationOfExpenditure}</h3>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <div className="expense-item__price">${props.amount}</div>
        </div>
     </div>
   );
