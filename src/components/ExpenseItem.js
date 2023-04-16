862ca4fa521e06bcc8e5805a84bcd1420e6315ca
Gourav <Gouravkumar.1273@gmail.com>
2023-04-16 07:23:05 +0530

    LocationOfExpenditure added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import "./ExpenseItem.css";
 const ExpenseItem = () => {
+  const expenseDate=new Date(2021,2,28);
+  const expenseTitle='Car Insurane';
+  const expenseAmount=294.67;
+  const LocationOfExpenditure='Delhi';
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toString()}</div>
        <div className="expense-item__description">
-        <h2>Car Insurane</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle} </h2>
+        <h3>{LocationOfExpenditure}</h3>
+        <div className="expense-item__price">${expenseAmount}</div>
        </div>
     </div>
   );
