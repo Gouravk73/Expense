6f4a05287fb549c682cb5179aea281b2a67a026a
Gourav <Gouravkumar.1273@gmail.com>
2023-04-16 08:25:19 +0530

    expenseDetail updated

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
+
 const ExpenseItem = (props ) => {
-  const month=props.date.toLocaleString('en-US',{month:'long'} );
-  const day=props.date.toLocaleString('en-US',{day:'2-digit'} );
-  const year=props.date.getFullYear();
-  const LocationOfExpenditure='Delhi';
+  
   console.log(props.date);
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
-       <div className="expense-item__description">
-        <h2>{props.title} </h2>
-        <h3>{LocationOfExpenditure}</h3>
-        <div className="expense-item__price">${props.amount}</div>
-       </div>
+      <ExpenseDate date={props.date} />
+
+      <ExpenseDetails  title={props.title} 
+      amount={props.amount} />
+
+
     </div>
   );
 }
