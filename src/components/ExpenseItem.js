43be74fa0f1467173422dac0aaac18705b69ee44
Gourav <Gouravkumar.1273@gmail.com>
2023-04-21 20:54:14 +0530

    add expense buttom

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 
 const ExpenseItem = (props ) => {
-  const clickHandler=() => {
-    
-    console.log('Clicked');
+  const[title,setTitle] =useState(props.title);
+  const[amount,setAmount] =useState(props.amount);
+
+  const clickHandler = () => {
+    setTitle('updated');
+    console.log(title);
   };
+  
   const clickDeleteHandler=() => {
     console.log('deleted');
+  };
+  const clickExpenseHandler=() => {
+    setAmount(amount+100);
   };
   console.log(props.date);
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
 
-      <ExpenseDetails  title={props.title} 
-      amount={props.amount} />
+      <ExpenseDetails  title={title} 
+      amount={amount} />
       <button onClick={clickHandler}>Change Title</button>
       <button onClick={clickDeleteHandler}>Delete Title</button>
+      <button onClick={clickExpenseHandler}>Add Expemse</button>
 
     </div>
   );
